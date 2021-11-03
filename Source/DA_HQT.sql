75c448dd0a1ecc741e2315e8883483a3e996d76a
KariusHan4401 <lkhanh4401@gmail.com>
2021-11-03 20:59:53 +0700

    Create database and user/role .sql

---
+++ Source/DA_HQT.sql
@@ -0,0 +1,25 @@
+﻿use QLHTChuyenHang
+go
+
+--Create login
+exec sp_addlogin 'cooperator1', 'cooperator1'
+exec sp_addlogin 'customer1', 'customer1'
+exec sp_addlogin 'driver1', 'driver1'
+exec sp_addlogin 'employee1', 'employee1'
+exec sp_addlogin 'admin1', 'admin1'
+
+--Create user
+create user cooperator1 for login cooperator1
+create user customer1 for login customer1
+create user driver1 for login driver1
+create user employee1 for login employee1
+create user admin1 for login admin1
+
+--Create Role
+exec sp_addrole 'Co-operator'
+exec sp_addrole 'Customer'
+exec sp_addrole 'Driver'
+exec sp_addrole 'Employee'
+exec sp_addrole 'Admin'
+
+--Grant authorization

8d744a00b3cf3b0d5194fd06e7deaae14770887c
thisislins <lephuonglinh169@gmail.com>
2021-11-30 17:22:30 +0700

    Fix Script lỗi - Linh

--- LINH_LỖI/1. Tran_1_Phantom.sql
+++ LINH_LỖI/1. Tran_1_Phantom.sql
@@ -1,6 +1,7 @@
 ﻿USE QLHTChuyenHang
 GO
 -- ĐỐI TÁC XEM DOANH THU --
+-- Đăng nhập: Username: Lily, Password: TK000027
 DECLARE @RT INT
 EXEC @RT = USP_DT_XEMDOANHTHU
 IF @RT = 1
--- LINH_LỖI/2. Tran_1_Phantom.sql
+++ LINH_LỖI/2. Tran_1_Phantom.sql
@@ -1,13 +1,12 @@
 ﻿USE QLHTChuyenHang
 GO
 -- KHÁCH HÀNG TÌM SẢN PHẨM --
+-- Tài khoản: Username: Keshi ; Password: TK000031
+
 DECLARE @FLAG INT
 EXEC @FLAG = USP_KH_TIMSP N'Vở'
 IF @FLAG = 1
 	PRINT N'CÓ LỖI XẢY RA!'
 ELSE
 	PRINT N'HIỂN THỊ DANH SÁCH SẢN PHẨM'
-
--- SELECT * FROM UV_XEMSP_KH
-
 
